fe2b130d7407b2ec22fe19378a832c8d0959022e
machageDev <machagefranklyn@gmail.com>
2025-09-10 00:34:12 +0300

    added template index.html to display list of polls

--- zApp/views.py
+++ zApp/views.py
@@ -5,7 +5,7 @@ from zApp.models import Question
 
 # Create your views here.
 def index(request):
-     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
     output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
