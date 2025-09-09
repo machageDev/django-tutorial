49ce60f6b52f1bebdb109375ba76a5eb13192f55
machageDev <machagefranklyn@gmail.com>
2025-09-09 23:02:32 +0300

    added models 2 of them

--- zApp/models.py
+++ zApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateField("date_published")
+
+class Choice (models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  
+    text_choice = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)  
+    
