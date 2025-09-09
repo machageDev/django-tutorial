16d71f484e59ce883ea63d5b647a2647b7295a80
machageDev <machagefranklyn@gmail.com>
2025-09-09 23:54:49 +0300

    writin views for app

--- zApp/views.py
+++ zApp/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world, ypu are amazing")
+
+def detail(request,question_id):
+    return HttpResponse("You are looking for question %S"% question_id)
+
+def result(request,question_id):
+    response = " You are looking at the result of question %s"
+    return HttpResponse(response %question_id)
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
