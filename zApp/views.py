d6586657eba55882af0c023b9841fb9438e91027
machageDev <machagefranklyn@gmail.com>
2025-09-09 23:59:14 +0300

    modified index view

--- zApp/views.py
+++ zApp/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from zApp.models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, ypu are amazing")
+     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request,question_id):
     return HttpResponse("You are looking for question %S"% question_id)
