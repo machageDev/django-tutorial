d18bc7c43fd25517da1b59b360cfac1ffe846ac8
machageDev <machagefranklyn@gmail.com>
2025-09-09 23:33:17 +0300

    added __str__ which is convinincw when dealing with intwrctive prompts

--- zApp/models.py
+++ zApp/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField("date_published")
+    def __str__(self):
+        return self.question_text
 
 class Choice (models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)  
     text_choice = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)  
+    def __str__(self):
+        return self.text_choice
     
