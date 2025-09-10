ce6af4f9ea9a6342074a7ebfeec8f8eb320da745
machageDev <machagefranklyn@gmail.com>
2025-09-10 23:49:39 +0300

    using display decorator

--- zApp/models.py
+++ zApp/models.py
@@ -17,4 +17,18 @@ class Choice (models.Model):
     votes = models.IntegerField(default=0)  
     def __str__(self):
         return self.text_choice
-    
+
+
+from django.contrib import admin
+
+
+class Question(models.Model):
+    # ...
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now    
