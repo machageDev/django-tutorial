ee169fd684ac047c985a6b383b9ea3ec35d665d8
machageDev <machagefranklyn@gmail.com>
2025-09-10 23:47:14 +0300

    customize admin panel in admin.py

--- zApp/admin.py
+++ zApp/admin.py
@@ -4,3 +4,27 @@ from django.contrib import admin
 from .models import Question , Choice
 admin.site.register(Question)
 admin.site.register(Choice)
+
+
+#customize admin panel
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date","question_test"]
+admin.site.register(Question, QuestionAdmin)
+    
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
