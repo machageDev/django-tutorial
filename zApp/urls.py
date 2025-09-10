7e09fed7ff2b39822bd62c15de78a1843f9d721e
machageDev <machagefranklyn@gmail.com>
2025-09-10 16:44:14 +0300

    using generic views

--- zApp/urls.py
+++ zApp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     
     path("<int:question_id>/", views.detail, name="detail"),
     
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/results/", views.result, name="results"),
     
     path("<int:question_id>/vote/", views.vote, name="vote"),
     
--- zApp/views.py
+++ zApp/views.py
@@ -7,6 +7,7 @@ from .models import Question,Choice
 from django.http import Http404
 from django.shortcuts import  get_object_or_404,render
 from django.db.models import F
+from django.views import generic
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -46,3 +47,28 @@ def vote(request,question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
     return HttpResponseRedirect(reverse("polls:results",args=(question_id)))
+
+def results(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"result.html",{"question":question})
+
+#using generic views
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+def vote(request, question_id):
+    # same as above, no changes needed.
+    ...    
